f533640bb610b41bedd04caebda35aad1b0ded8c
richardgarciaordaz <64115217+richardgarciaordaz@users.noreply.github.com>
2023-12-29 17:19:33 +0000

    Personajes

---
+++ src/js/component/personajes.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.js";
+
+export const Personajes = () => {
+    const { store, actions } = useContext(Context)
+
+    useEffect(() => {
+        actions.obtenerPersonajes()
+    }, [])
+    return (
+        <div className="container">
+            <h1> Personajes  </h1>
+            <div className="card-group">
+                <div className="d-flex flex-row overflow-scroll">
+                    {store.personajes.map((item, id) => (
+
+
+                        <div class="card" style={{ width: "18rem", flex: "none", margin: "10px" }} key={id}>
+                            <img src={"https://starwars-visualguide.com/assets/img/characters/" + (id + 1) + ".jpg"} class="card-img-top" alt="..." />
+                            <div class="card-body">
+                                <h5 class="card-title">{item.name} </h5>
+                                <p class="card-text">Gender: {item.gender}</p>
+                                <p class="card-text">Eye Color: {item.eye_color}</p>
+                                <a href="#" class="btn btn-primary">More info</a>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+};
